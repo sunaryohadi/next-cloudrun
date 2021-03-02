ab4e028282c94ab59d821fc34eccb39d8472dd85
sunaryohadi <sunaryohadi@gmail.com>
2021-03-02 09:01:42 +0700

    Base

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="w-full min-h-screen flex flex-col">
+      <header>Home</header>
+      <main className="flex-1 bg-red-50">Main</main>
+      <footer>Footer</footer>
+    </div>
+  );
+}
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './commons/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './screens/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
